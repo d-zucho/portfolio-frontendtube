437177c3a14195408f5eb46ffb653ac460708e7c
D_Zucho <danny.nico.dev@outlook.com>
2024-08-27 19:23:51 -0700

    add responsiveness to hero section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 
 export const HeroSection = () => {
   return (
-    <section className='py-32'>
+    <section className='py-32 md:py-48 lg:py-60'>
       <div className='container'>
         <div className='flex flex-col items-center'>
           <Image
@@ -13,26 +13,30 @@ export const HeroSection = () => {
             width={100}
             height={100}
           />
-          <div className='bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex'>
-            <div></div>
+          <div className='bg-gray-950 gap-4 rounded-lg border border-gray-800 px-4 py-1.5 inline-flex items-center'>
+            <div className='size-2.5 rounded-full bg-green-500'></div>
             <div className='text-sm font-medium'>
               Available for new projects
             </div>
           </div>
         </div>
-        <h1>Building Exceptional User Experiences</h1>
-        <p>
-          I specialize in transforming designs into functional, high-performing
-          web applications. Let's discuss your next project.
-        </p>
-        <div>
-          <button>
-            <span>Explore My Work</span>
-            <ArrowDown />
+        <div className='max-w-lg mx-auto'>
+          <h1 className='font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide'>
+            Building Exceptional User Experiences
+          </h1>
+          <p className='mt-4 text-center text-white/60 md:text-lg'>
+            I specialize in transforming designs into functional,
+            high-performing web applications. Let's discuss your next project.
+          </p>
+        </div>
+        <div className='flex flex-col md:flex-row justify-center items-center mt-8 gap-4'>
+          <button className='inline-flex gap-2 px-6 h-12 border border-white/15 rounded-xl items-center'>
+            <span className='font-semibold'>Explore My Work</span>
+            <ArrowDown className='size-4' />
           </button>
-          <button>
+          <button className='flex gap-2 items-center border border-white bg-white text-gray-900 h-12 px-6 rounded-xl'>
             <span>👋🏼</span>
-            <span>Let&apos;s Connect</span>
+            <span className='font-semibold'>Let&apos;s Connect</span>
           </button>
         </div>
       </div>

3d712c12c791c36dab3ec7fb46f5298da5becddd
D_Zucho <danny.nico.dev@outlook.com>
2024-08-27 21:42:42 -0700

    add stars and sparkles

--- src/components/HeroOrbit.tsx
+++ src/components/HeroOrbit.tsx
@@ -9,6 +9,7 @@ interface OrbitProps {
 export const HeroOrbit = ({ children, size, rotation }: OrbitProps) => (
   <div className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2'>
     <div
+      className='flex justify-start items-start'
       style={{
         transform: `rotate(${rotation}deg)`,
         width: size,
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import grainImage from '@/assets/images/grain.jpg'
 import { HeroOrbit } from '@/components/HeroOrbit'
 import StarIcon from '@/assets/icons/star.svg'
+import SparkleIcon from '@/assets/icons/sparkle.svg'
 
 export const HeroSection = () => {
   return (
@@ -28,10 +29,33 @@ export const HeroSection = () => {
         <StarIcon className='size-28 text-emerald-300' />
       </HeroOrbit>
       <HeroOrbit size={550} rotation={20}>
-        <StarIcon className='size-12 text-emerald-300 border border-red-400' />
+        <StarIcon className='size-12 text-emerald-300' />
       </HeroOrbit>
       <HeroOrbit size={590} rotation={98}>
-        <StarIcon className='size-8 text-emerald-300 border border-red-400' />
+        <StarIcon className='size-8 text-emerald-300' />
+      </HeroOrbit>
+
+      {/* sparkles */}
+      <HeroOrbit size={430} rotation={-14}>
+        <SparkleIcon className='size-8 text-emerald-300/20' />
+      </HeroOrbit>
+      <HeroOrbit size={440} rotation={79}>
+        <SparkleIcon className='size-5 text-emerald-300/20' />
+      </HeroOrbit>
+      <HeroOrbit size={530} rotation={178}>
+        <SparkleIcon className='size-10 text-emerald-300/20' />
+      </HeroOrbit>
+      <HeroOrbit size={710} rotation={144}>
+        <SparkleIcon className='size-14 text-emerald-300/20' />
+      </HeroOrbit>
+      <HeroOrbit size={724} rotation={85}>
+        <div className='size-3 bg-emerald-300/10 rounded-full' />
+      </HeroOrbit>
+      <HeroOrbit size={520} rotation={-41}>
+        <div className='size-2 bg-emerald-300/20 rounded-full' />
+      </HeroOrbit>
+      <HeroOrbit size={648} rotation={-5}>
+        <div className='size-2 bg-emerald-300/20 rounded-full' />
       </HeroOrbit>
 
       <div className='container'>

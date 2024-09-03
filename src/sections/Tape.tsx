7dd83011cbd3c2b709bef367653ae69337cbdb1f
D_Zucho <danny.nico.dev@outlook.com>
2024-09-03 02:36:19 -0700

    add tape continous scroll loop animation

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -1,4 +1,5 @@
 import StarIcon from '@/assets/icons/star.svg'
+import { Fragment } from 'react'
 
 const words = [
   'Performant',
@@ -19,14 +20,18 @@ export const TapeSection = () => {
     <div className='py-16 lg:py-24 overflow-clip'>
       <div className='bg-gradient-to-r from-emerald-300 to-sky-400 overflow-x-clip -rotate-3 -mx-1'>
         <div className='flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]'>
-          <div className='flex flex-none gap-4 py-3'>
-            {words.map((word, index) => (
-              <div key={index} className='inline-flex gap-4 items-center'>
-                <span className='text-gray-900 font-extrabold text-sm uppercase'>
-                  {word}
-                </span>
-                <StarIcon className='size-6 text-gray-900 -rotate-12' />
-              </div>
+          <div className='flex flex-none gap-4 py-3 pr-4 animate-move-left [animation-duration:30s]'>
+            {[...new Array(2)].fill(0).map((_, index) => (
+              <Fragment key={index}>
+                {words.map((word, index) => (
+                  <div key={index} className='inline-flex gap-4 items-center'>
+                    <span className='text-gray-900 font-extrabold text-sm uppercase'>
+                      {word}
+                    </span>
+                    <StarIcon className='size-6 text-gray-900 -rotate-12' />
+                  </div>
+                ))}
+              </Fragment>
             ))}
           </div>
         </div>

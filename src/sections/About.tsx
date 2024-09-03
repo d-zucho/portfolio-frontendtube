88e7c1bbbb312e7bcf0ed66fcc4b4a66af058045
D_Zucho <danny.nico.dev@outlook.com>
2024-09-02 18:25:09 -0700

    finish call to action section

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -69,26 +69,26 @@ const hobbies = [
 
 export const AboutSection = () => {
   return (
-    <section className='mt-20 pb-96 container'>
+    <section className='py-20 lg:py-28  container'>
       <SectionHeader
         title='A Glimpse into My World'
         subtitle='About Me'
         description=' Learn more about who I am, what I do, and what inspires me.'
       />
       <div className='mt-20 flex flex-col gap-8'>
-        <div className='grid grid-cols-1 gap-y-8 md:gap-x-8 md:grid-cols-5'>
+        <div className='grid grid-cols-1 gap-y-8 md:gap-x-8 md:grid-cols-5 lg:grid-cols-3'>
           {/* MY READS CARD */}
-          <Card className='md:col-span-2 h-[320px]'>
+          <Card className='md:col-span-2 lg:col-span-1 h-[320px]'>
             <CardHeader
               title='My Reads'
               description='Explore the books shaping my perspectives.'
             />
-            <div className='w-40 mx-auto mt-2'>
+            <div className='w-40 mx-auto mt-2 md:mt-0'>
               <Image src={bookImage} alt='Book Cover' />
             </div>
           </Card>
           {/* TOOLBOX CARD */}
-          <Card className='h-[320px] col-span-3'>
+          <Card className='h-[320px] col-span-3 lg:col-span-2'>
             <CardHeader
               title='My Toolbox'
               description='Explore the technologies and tools I use to craft exceptional digital experiences.'
@@ -103,44 +103,50 @@ export const AboutSection = () => {
           </Card>
         </div>
 
-        {/* HOBBIES CARD */}
-        <Card className='h-[320px] p-0 flex flex-col'>
-          {/* <div className='px-6 py-6'>
-            <StarIcon className='size-6 text-emerald-300' />
-            <h3>Beyond the Code</h3>
-            <p>Explore my interests and hobbies beyond the digital realm</p>
-          </div> */}
-          <CardHeader
-            title='Beyond the Code'
-            description='Explore my interests and hobbies beyond the digital realm'
-            className='p-6'
-          />
-          <div className='relative flex-1'>
-            {hobbies.map((hobby) => (
-              <div
-                key={hobby.title}
-                className='inline-flex items-center gap-2 px-6 py-1.5 bg-gradient-to-r from-emerald-300 to-sky-400 rounded-full absolute'
-                style={{ left: hobby.left, top: hobby.top }}
-              >
-                <span className='font-medium text-gray-950'>{hobby.title}</span>
-                <span>{hobby.emoji}</span>
-              </div>
-            ))}
-          </div>
-        </Card>
-
-        {/* MAP CARD */}
-        <Card className='h-[320px] relative p-0 flex items-center justify-center'>
-          <Image
-            src={mapImage}
-            alt='Map'
-            className='h-full w-full object-cover object-left-top'
-          />
-
-          <div className='absolute rounded-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-gradient-to-r from-emerald-300 to-sky-400 after:content-[""] after:absolute after:inset-0 after:outline after:outline-2 after:-outline-offset-2 after:rounded-full after:outline-gray-950/30'>
-            <Image src={smileMemoji} alt='Smiling Memoji' className='size-20' />
-          </div>
-        </Card>
+        <div className='grid grid-cols-1 md:grid-cols-5 lg:grid-cols-3 gap-y-8 md:gap-x-8'>
+          {/* HOBBIES CARD */}
+          <Card className='h-[320px] p-0 flex flex-col md:col-span-3 lg:col-span-2'>
+            {/* <div className='px-6 py-6'>
+              <StarIcon className='size-6 text-emerald-300' />
+              <h3>Beyond the Code</h3>
+              <p>Explore my interests and hobbies beyond the digital realm</p>
+            </div> */}
+            <CardHeader
+              title='Beyond the Code'
+              description='Explore my interests and hobbies beyond the digital realm'
+              className='p-6'
+            />
+            <div className='relative flex-1'>
+              {hobbies.map((hobby) => (
+                <div
+                  key={hobby.title}
+                  className='inline-flex items-center gap-2 px-6 py-1.5 bg-gradient-to-r from-emerald-300 to-sky-400 rounded-full absolute'
+                  style={{ left: hobby.left, top: hobby.top }}
+                >
+                  <span className='font-medium text-gray-950'>
+                    {hobby.title}
+                  </span>
+                  <span>{hobby.emoji}</span>
+                </div>
+              ))}
+            </div>
+          </Card>
+          {/* MAP CARD */}
+          <Card className='h-[320px] relative p-0 flex items-center justify-center md:col-span-2 lg:col-span-1'>
+            <Image
+              src={mapImage}
+              alt='Map'
+              className='h-full w-full object-cover object-left-top'
+            />
+            <div className='absolute rounded-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-gradient-to-r from-emerald-300 to-sky-400 after:content-[""] after:absolute after:inset-0 after:outline after:outline-2 after:-outline-offset-2 after:rounded-full after:outline-gray-950/30'>
+              <Image
+                src={smileMemoji}
+                alt='Smiling Memoji'
+                className='size-20'
+              />
+            </div>
+          </Card>
+        </div>
       </div>
     </section>
   )
--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,3 +1,36 @@
+import ArrowUpRightIcon from '@/assets/icons/arrow-up-right.svg'
+import grainImage from '@/assets/images/grain.jpg'
+
 export const ContactSection = () => {
-  return <div>Contact Section</div>;
-};
+  return (
+    <section className='py-16 pt-12 lg:mt-8'>
+      <div className='container'>
+        <div className='bg-gradient-to-r from-emerald-300 to-sky-400 text-gray-900 py-8 px-10 text-center rounded-3xl relative overflow-hidden z-0'>
+          {/* grain bg */}
+          <div
+            className='absolute inset-0 -z-10 opacity-5'
+            style={{
+              backgroundImage: `url(${grainImage.src})`,
+            }}
+          />
+          <div className='flex flex-col gap-8 md:gap-16 md:flex-row items-center md:justify-between'>
+            <div className='md:text-left md:max-w-sm lg:max-w-none'>
+              <h2 className='text-2xl md:text-3xl font-medium font-serif'>
+                Let's create something amazing together
+              </h2>
+              <p className='text-sm md:text-base mt-2'>
+                Ready to bring your next project to life? Let's connect and
+                discuss how I can help you achieve your goals.
+              </p>
+            </div>
+
+            <button className='inline-flex gap-2 items-center text-white bg-gray-900 px-6 h-12 rounded-xl border-gray-900 border-2'>
+              <span className='font-semibold'>Contact Me</span>
+              <ArrowUpRightIcon className='size-4' />
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
